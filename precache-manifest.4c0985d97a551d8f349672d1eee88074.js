123502d1452ca523c192f61037a57c2e80da7b83
Misha K3ler <ikeler1@gmail.com>
2020-06-18 08:10:47 +0300

    Updates

---
+++ precache-manifest.4c0985d97a551d8f349672d1eee88074.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea34bf4009dae7097aa8f1c733ca6dd",
+    "url": "/StarNavi_game/index.html"
+  },
+  {
+    "revision": "6d1b5dce044be40c538c",
+    "url": "/StarNavi_game/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7ac92fc4b641d97aff2c",
+    "url": "/StarNavi_game/static/css/main.59aeb27b.chunk.css"
+  },
+  {
+    "revision": "6d1b5dce044be40c538c",
+    "url": "/StarNavi_game/static/js/2.a91f2c4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/StarNavi_game/static/js/2.a91f2c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac92fc4b641d97aff2c",
+    "url": "/StarNavi_game/static/js/main.08df0966.chunk.js"
+  },
+  {
+    "revision": "fb9d9e3de6b7b1c88aee",
+    "url": "/StarNavi_game/static/js/runtime-main.8dcec400.js"
+  }
+]);
